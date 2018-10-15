48fe0ae60f94b3446cf22f7912695e65efcda435
Viacheslav <xrw.null@gmail.com>
2018-10-15 02:06:11 -0700

    dockerclient test

--- integtest/src/test/scala/acinonyx/integtest/MyTest.scala
+++ integtest/src/test/scala/acinonyx/integtest/MyTest.scala
@@ -1,5 +1,7 @@
 package acinonyx.integtest
 
+import com.spotify.docker.client.DefaultDockerClient
+import com.spotify.docker.client.messages.ContainerConfig
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -8,6 +10,15 @@ import org.scalatest.junit.JUnitRunner
 class MyTest extends FunSuite{
 
   test("check"){
+    val docker = DefaultDockerClient.fromEnv().build()
+
+    val cfg = ContainerConfig.builder()
+      .image("acinonyx-client:1.0.0")
+      .build()
+
+    val container = docker.createContainer(cfg)
+    docker.startContainer(container.id())
+
     assert(1 == 1)
   }
 }
