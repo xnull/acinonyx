3a6218fc2dbd2c88a01d0886d699b126c83b5b32
Viacheslav <xrw.null@gmail.com>
2018-12-08 03:23:18 -0800

    terraform, gradle 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
+import java.nio.file.Paths
+
 apply {
     from("$rootDir/gradle/idea.gradle.kts")
+}
+
+tasks.create("terraform", Exec::class) {
+    workingDir = Paths.get(rootDir.absolutePath, "script", "terraform").toFile()
+
+    commandLine = listOf("sh", "-c", "docker run --rm hashicorp/terraform:light -version")
 }
 
--- script/terraform/tf.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -i -t hashicorp/terraform:light version
